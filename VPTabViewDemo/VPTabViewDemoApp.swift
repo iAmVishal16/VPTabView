ee0792cb56e05e9df0f222cee33aeed27a93d249
Vishal Paliwal <paliwalvishal16@gmail.com>
2021-07-30 15:39:22 +0530

    Initial Project Setup

---
+++ VPTabViewDemo/VPTabViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VPTabViewDemoApp.swift
+//  VPTabViewDemo
+//
+//  Created by Vishal Paliwal on 30/07/21.
+//
+
+import SwiftUI
+
+@main
+struct VPTabViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
