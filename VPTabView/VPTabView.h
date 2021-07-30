315baa36bfaad39dcd233deda3ed639d5848442f
Vishal Paliwal <paliwalvishal16@gmail.com>
2021-07-30 15:28:54 +0530

    Initial Commit

---
+++ VPTabView/VPTabView.h
@@ -0,0 +1,18 @@
+//
+//  VPTabView.h
+//  VPTabView
+//
+//  Created by Vishal Paliwal on 30/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VPTabView.
+FOUNDATION_EXPORT double VPTabViewVersionNumber;
+
+//! Project version string for VPTabView.
+FOUNDATION_EXPORT const unsigned char VPTabViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VPTabView/PublicHeader.h>
+
+
